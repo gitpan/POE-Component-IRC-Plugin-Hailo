fee8453b7dd03381379d48fa4cebb6754c4fc82d
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2011-05-15 14:36:30 +0000

    Import of HINRIK/POE-Component-IRC-Plugin-Hailo-0.16 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-Hailo
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         HINRIK/POE-Component-IRC-Plugin-Hailo-0.16.tar.gz
    gitpan-cpan-author:       HINRIK
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/Hailo.pm
+++ lib/POE/Component/IRC/Plugin/Hailo.pm
@@ -3,7 +3,7 @@ BEGIN {
   $POE::Component::IRC::Plugin::Hailo::AUTHORITY = 'cpan:HINRIK';
 }
 BEGIN {
-  $POE::Component::IRC::Plugin::Hailo::VERSION = '0.15';
+  $POE::Component::IRC::Plugin::Hailo::VERSION = '0.16';
 }
 
 use strict;
